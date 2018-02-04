88419781546ace1469c39ba7bf62b4146b71ddc8
root <root@tecweb.lxd>
2018-02-04 20:10:13 +0000

    Varie funzioni per apt

--- systemcalls/callfromexternal.py
+++ systemcalls/callfromexternal.py
@@ -1,17 +1,11 @@
 from user import getusers, getuser, getgroups, addusertogroups, removeuserfromgroups, adduser, removeuser, updateuserpass, updateusershell
-from apps import listinstalled, aptsearch, aptshow
+from apps import listinstalled, aptsearch, aptshow, aptinstall
 
 
-
-
-p1 = aptshow('sudo')
-print(type(p1))
-p1 = p1.split('\n\n')
-
-for i in p1:
-    print(i)
+aptinstall('htop')
 
 #TODO: non cancellare le righe successive, discutere con Lucia dei Keyword Arguments
+exit()
 for i in listinstalled( summary=True ):
     print(i)
 
