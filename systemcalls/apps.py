f36843f1774ddce858970803520ced8f8261d51c
root <root@tecweb.lxd>
2018-02-04 12:03:29 +0000

    Ricerca pacchetti

--- systemcalls/apps.py
+++ systemcalls/apps.py
@@ -7,6 +7,8 @@ import pprint
 import inspect
 #import urllib.parse
 
+import ast
+
 
 #Se icludesummary è True allora aggiunge alla lista restituita anche le informazioni sull'applicazione
 def listinstalled( summary=False ):
@@ -23,36 +25,41 @@ def listinstalled( summary=False ):
     if summary: keys.append('summary')
     
     #apps conterrà la lista di dizionari con tutte le app da restituire
-    apps = list()
+    pkgs = list()
 
     #Inserimento valori nella lista apps()
     for i in output:
         appinfo = i.split(';')
-        apps.append( dict( zip(keys, appinfo) ) )
+        pkgs.append( dict( zip(keys, appinfo) ) )
 
-    return apps
+    return pkgs
 
 
-def aptsearch( appname, namesonly=True ):
 
-    command = ['apt-cache', 'search', appname]
+#Ricerca di una applicazione. se namesonly è true (default) la ricerca viene effettuata solo nel nome del pacchetto
+def aptsearch( pkgname, namesonly=True ):
+
+    command = ['apt-cache', 'search', pkgname]
     if namesonly: command.append('--names-only')
 
     p1 = Popen(command, stdout=PIPE, universal_newlines=True).communicate()[0]
     output = p1.splitlines()
 
     keys = ['name', 'desc']
-    apps = list()
+    pkgs = list()
 
     for i in output:
         appinfo = i.split(' - ')
-        apps.append( dict( zip(keys, appinfo) ) )
+        pkgs.append( dict( zip(keys, appinfo) ) )
 
-    return apps
+    return pkgs
 
-def aptshow():
+def aptshow(pkgname):
     
+    command = ['apt-cache', 'show', pkgname]
     p1 = Popen(command, stdout=PIPE, universal_newlines=True).communicate()[0]
+
+    return p1
     
 
 
--- systemcalls/callfromexternal.py
+++ systemcalls/callfromexternal.py
@@ -1,14 +1,15 @@
 from user import getusers, getuser, getgroups, addusertogroups, removeuserfromgroups, adduser, removeuser, updateuserpass, updateusershell
-from apps import listinstalled, aptsearch
+from apps import listinstalled, aptsearch, aptshow
 
 
 
-for i in listinstalled(summary=False):
+
+p1 = aptshow('sudo')
+print(type(p1))
+p1 = p1.split('\n\n')
+
+for i in p1:
     print(i)
-
-exit()
-
-
 
 #TODO: non cancellare le righe successive, discutere con Lucia dei Keyword Arguments
 for i in listinstalled( summary=True ):
