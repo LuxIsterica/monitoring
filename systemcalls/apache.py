ebe76d072c904fba89a3f67ce1a71dfb11b7340f
gisk8er@gmail.com <gisk8er@gmail.com>
2018-03-14 18:28:21 +0000

    A bunch of apache functions

--- systemcalls/apache.py
+++ systemcalls/apache.py
@@ -11,33 +11,107 @@ import inspect
 def getvhosts():
 
     apacheconfdir = "/etc/apache2/"
+    availabledir = 'sites-available/'
+    #There no need for 'sites-enabled' because we can find those vhosts in sites-available too
 
-    #Gets enable vhosts
-    enabled = set( os.listdir(apacheconfdir + 'sites-enabled') )
-    #Gets nonactive vhosts
-    notactive = set( os.listdir(apacheconfdir  + 'sites-available') ).difference(enabled)
-    
     vhosts = list()
 
+    #Getting enable vhosts and appending to vhosts list as dictionary
+    enabled = set( os.listdir(apacheconfdir + 'sites-enabled') )
     for vhost in enabled:
+        vhosts.append({ 'filename': vhost, 'active': 1 })
+
+    #Gets nonactive vhosts and appending to vhosts list as dictionary
+    notactive = set( os.listdir(apacheconfdir  + 'sites-available') ).difference(enabled)
+    for vhost in notactive:
+        vhosts.append({ 'filename': vhost, 'active': 0 })
 
 
-        vhostspecs = { 'filename': vhost, 'active': 1 }
+    
+    #Filling the list to return with all information about every single vhost
+    for vhost in vhosts:
 
-        with open(apacheconfdir + 'sites-enabled/' + vhost) as opened:
+        #"vhostcontent" mantains all vhost file content
+        with open(apacheconfdir + availabledir + vhost['filename']) as opened:
             vhostcontent = opened.read().splitlines()
 
-
+        
+        i = 0
         for line in vhostcontent:
             line = line.lstrip()
-            linestosearch = ('DocumentRoot', 'ServerName', 'ServerAlias')
-            print(line)
+            linestosearch = ('Alias', 'DocumentRoot', 'ServerName', 'ServerAlias')
 
+            #If any of this words in vhost file add the entire splitted line to the vhost dict
             if any( line.startswith(s) for s in linestosearch ):
-                print('QUI' + line)
-                vhostspecs.update({ line.split() })
-        vhosts.append( vhostspecs )
 
-#    for line in notactive: vhosts.append({ 'filename': line, 'active': 0 })
+                #A vhost can handle multiple ServerAlias but dict() cannot accept multiple key with the same string
+                #so we're going to add an incremental number to the key "ServerAlias"
+                if 'ServerAlias' in  line: line = re.sub('ServerAlias', 'ServerAlias' + str(i), line)
+                i += 1
+
+                line = line.split(None, maxsplit=1)
+                #vhost dict is a pointer to the original dict in vhosts list, hence an update here means an update to the original dict
+                vhost.update({ line[0]: line[1] })
+
 
     return vhosts
+
+
+
+
+
+def apache2(op="status"):
+    
+    #Can only accept these parameters
+    acceptedparams = ['stop', 'status', 'reload', 'restart']
+    if not any(op in param for param in acceptedparams):
+        return {'errcode': -1, 'errmessage': 'Bad parameter :' + op}
+    else:
+        command = ['systemctl', op, 'apache2']
+
+
+    toreturn = None
+    try:
+        if op is "status":
+            #Avoid to print journal (log) lines in output
+            command.append('-n0')
+
+            #We are using Popen here because check_output fails to return stdout and stderr on exit code != 0
+            toreturn = Popen(command, stdout=PIPE, universal_newlines=True).communicate()[0].splitlines()
+
+            #Filtering useless lines
+            linestomantain = ['active', 'memory', 'cpu']
+            toreturn = list( filter( lambda line: any(s in line.lower() for s in linestomantain), toreturn ) )
+            #Formatting output
+            toreturn = dict( map(lambda line: line.lstrip().split(':', maxsplit=1), toreturn ) )
+
+        else:
+            #Logging operation to MongoDB; in this specific case "toreturn" contains mongo logid
+            toreturn = mongolog( locals() )
+            check_call(command)
+    except CalledProcessError:
+        pass
+
+
+    return toreturn
+
+
+
+#NOTE: Must not be called directly
+def managevhosts(filename, op):
+    logid = mongolog( locals() )
+
+    command = [op, filename]
+
+    try:
+        check_output = check_call(command)
+    except CalledProcessError as e:
+        return command_error(e, command)
+
+    return logid
+    apache2(op='reload')
+    #Reloading apache after site activation
+
+#Call a function with different parameters
+def activatevhost(filename): managevhosts(filename, op='a2ensite')
+def deactivatevhost(filename): managevhosts(filename, op='a2dissite')
--- systemcalls/callfromexternal.py
+++ systemcalls/callfromexternal.py
@@ -2,13 +2,14 @@ from user import getusers, getuser, getgroups, addusertogroups, removeuserfromgr
 from apps import listinstalled, aptsearch, aptshow, aptinstall, aptremove, aptupdate, addrepo,  getexternalrepos, removerepofile
 from system import hostname, etchosts, getsysteminfo
 from systemfile import updatedb, locate, getsetfile, removefile
-from apache import getvhosts
+from apache import getvhosts, apache2, activatevhost, deactivatevhost
 from pprint import pprint
 import os
 
 
 
-pprint( getvhosts() )
+
+pprint( activatevhost('default-ssl') ) #test
 
 #TODO: non cancellare le righe successive, discutere con Lucia dei Keyword Arguments
 exit()
