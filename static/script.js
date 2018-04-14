c5f340858fa791dbb5eae5fb521a8b9ede99bd07
Lucia <lucia.polizzi1992@gmail.com>
2018-04-14 12:27:36 +0200

    application,file,user e grafica

--- static/script.js
+++ static/script.js
@@ -1,5 +1,10 @@
+var newShell = null;
 $(document).ready(function () {
-                 $('#sidebarCollapse').on('click', function () {
-                     $('#sidebar').toggleClass('active');
-                 });
-             });
+	$('#sidebarCollapse').on('click', function () {
+		$('#sidebar').toggleClass('active');
+	});
+
+	function removeAll(){
+		document.getElementById("pathFileFound").innerHTML = "";
+	}
+});
