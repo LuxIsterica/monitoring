ca5199030b273c07d22f6c67332f47850dede2a2
LuxIsterica <lucia.polizzi1992@gmail.com>
2018-04-28 13:14:09 +0200

    aggiunta funzione hostname e uodate hostname

---
+++ systemcalls/prova.py
@@ -0,0 +1,28 @@
+from user import getusers, getuser, getgroups, getusergroups, getusernotgroups, addusertogroups, removeuserfromgroups, adduser, removeuser, updateuserpass, getshells, updateusershell
+from apps import listinstalled, aptsearch, aptshow, aptinstall, aptremove, aptupdate, addrepo,  getexternalrepos, removerepofile
+from system import hostname, getsysteminfo
+from systemfile import updatedb, locate, removefile
+from apache import getvhosts, getmods, getconf, activatevhost, deactivatevhost, apachestatus, apachereload
+from pprint import pprint
+from utilities import filediff, filedit
+from network import ifacestat, getnewifacealiasname, ifacedown, ifaceup, editiface, createalias, destroyalias, getroutes, addroute, defaultroute, delroute
+from cron import getusercron, writeusercrontab
+import os
+import sys
+
+
+
+data = getsysteminfo()
+#data = writeusercrontab('giuseppe', 'nuovociaone')
+if data['returncode'] is 0:
+    (cpu,mem,proc) = data['data']
+
+pprint(proc)
+
+
+#TODO: non cancellare le righe successive, discutere con Lucia dei Keyword Arguments
+exit()
+for i in listinstalled( summary=True ):
+    print(i)
+
+removeuser('temp', removehome=True)
--- template.py
+++ template.py
@@ -3,7 +3,7 @@ sys.path.append('systemcalls')
 from user import getusers, getuser, getgroups, getshells, updateusershell
 from apps import listinstalled, aptsearch
 from systemfile import locate
-from system import getsysteminfo
+from system import getsysteminfo, hostname
 from network import ifacestat
 from flask import Flask, render_template, flash, request, redirect, url_for
 from flask_bootstrap import Bootstrap
@@ -15,33 +15,36 @@ bootstrap = Bootstrap(app)
 
 ##### FUNZIONALITÀ user.py #####
 
-#http://localhost:5000/
+#http://localhost:5000/dash
 # definizione base dash con componente fissa navbar
 @app.route('/dash')
 def dash():
-	error = None
-	tupl = getsysteminfo()
-	if tupl['returncode'] != 0:
-		flash(tupl['stderr'])
-	else:
-		(cpu,mem,proc) = tupl['data']
-		return render_template('dash.html', cpu=cpu,mem=mem,proc=proc)
-	
-	return render_template('dash.html')
+#	error = None
+#	tpl = getsysteminfo()
+#	if tpl['returncode'] != 0:
+#		flash(tpl['stderr'])
+#	else:
+#		(cpu,mem,proc) = tpl['data']
+#		return render_template('dash.html', cpu = cpu, mem = mem, proc = proc)
+
+#	return render_template('dash.html')
+	tpl = getsysteminfo()
+	(cpu,mem,proc) = tpl['data']
+	return render_template('dash.html', cpu = cpu, mem = mem, proc = proc)
 
 # http://localhost:5000/listUser/
 @app.route('/listUserAndGroups')
 def listUserAndGroups():
 	users = getusers()
 	groups = getgroups()
-	return render_template('users.html',users = users,groups = groups)
+	return render_template('users.html', users = users,groups = groups)
 
 # http://localhost:5000/getInfoUser/<clicca valore uname>
 @app.route('/getInfoUser/<string:uname>')
 def getInfoUser(uname):
 	infouser = getuser(uname)
 	shells = getshells()
-	return render_template('info-user.html',infouser = infouser, shells = shells)
+	return render_template('info-user.html', infouser = infouser, shells = shells)
 
 #@app.route('/listShell')
 #def listShell():
@@ -72,7 +75,7 @@ def updateShell():
 @app.route('/listInstalled')
 def listInstalled():
 	listAppInst = listinstalled(True)
-	return render_template('applications.html',listAppInst = listAppInst)
+	return render_template('applications.html', listAppInst = listAppInst)
 
 @app.route('/findPkgInstalled', methods=['POST'])
 def findPkgInstalled():
@@ -82,7 +85,7 @@ def findPkgInstalled():
 		flash('Operazione errata')
 		return redirect(url_for('listInstalled'))
 	appFound = aptsearch(pkg)
-	return render_template('find-pkg-installed.html',appFound = appFound)
+	return render_template('find-pkg-installed.html', appFound = appFound)
 
 ##### FUNZIONALITÀ file.py #####
 @app.route('/file')
@@ -99,6 +102,33 @@ def findFile():
 	return render_template('file.html', pathFileFound = pathFileFound)
 
 ##### FUNZIONALITÀ system.py #####
+@app.route('/param')
+def param():
+	error = None
+	hname = hostname()
+	if(hname['returncode'] != 0):
+		flash(hname['stderr'])
+		flash(hname['command'])
+	else:
+		return render_template('param.html', hname=hname)
+
+	return render_template('param.html')
+
+@app.route('/newHostname', methods=['POST'])
+def newHostname():
+	error = None
+	hname = request.form['newHname'];
+	if not hname:
+		error = 'Hostname non può essere vuoto!'
+	else:
+		log = hostname(hname)
+	if(log['returncode'] != 0):
+		flash(log['stderr'])
+		flash(log['command'])
+	else:
+		flash('Hostname modificato correttamente')
+	
+	return redirect(url_for('param'))
 
 ##### FUNZIONALITÀ systemfile.py #####
 
@@ -106,7 +136,7 @@ def findFile():
 @app.route('/network')
 def network():
 	facestat = ifacestat()
-	return render_template('net.html',facestat=facestat)
+	return render_template('net.html', facestat=facestat)
 
 if __name__ == '__main__':
 	app.run(debug = True)
