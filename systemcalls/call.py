b268a83b7161758b6429482650eba64d6ea5e631
gisk8er@gmail.com <gisk8er@gmail.com>
2018-06-23 10:16:28 +0000

    Bug fixes

--- systemcalls/call.py
+++ systemcalls/call.py
@@ -1,7 +1,10 @@
 from pprint import pprint
 from network import ifacestat
 from logs import getlog
+from apps import aptshow
 
 
+pprint( aptshow('vim') )
 
+exit()
 pprint( getlog( dategte="2018-06-13", datelte="2018-06-14" )['data'] )
---
+++ systemcalls/info.py
@@ -0,0 +1,2 @@
+apacheinfo = "Apache is a web server to make it available for selected users or everyone your applications."
+
